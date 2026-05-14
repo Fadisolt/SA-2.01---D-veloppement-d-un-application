33be39fa8793476c81d1ffd08ad1dc62db74427a
idirn <idir.nait-ali@etu.u-paris.fr>
2026-05-14 18:34:57 +0200

    Modification du html : ajout de la selction des homonyme et du mode de jeu. Modification du app.js : fonction pour récuperer les valeurs selectionnées dans le menu. Modification du Game.js : Legers changements d'adaptation dans la fonction startGame.

--- 2025-S2.01-GettingStarted-main/js/app.js
+++ 2025-S2.01-GettingStarted-main/js/app.js
@@ -8,13 +8,19 @@ const game = new Game();
 
 document.querySelector('.game-form').addEventListener('submit', async function (event) {
   event.preventDefault();
-  // Todo À compléter
+  const configGame = {
+    playerName: document.querySelector('#playerName').value,
+    total: parseInt(document.querySelector('#difficulty').value),
+    collection: document.querySelector('#collection').value,
+    homonymes: parseInt(document.querySelector('#homonymes').value),
+    mode: document.querySelector('#mode').value
+  };
 
   try {
     // Todo Spécifier les paramètres de createGame()
-    const data = await ApiService.createGame();
+    const data = await ApiService.createGame(configGame.playerName,configGame.total);
     console.log('Success:', data, data.id);
-    game.startGame(data.id);
+    game.startGame(data.id, configGame);
   } catch (error) {
     console.error('Error:', error);
     alert(error.message || 'Erreur lors de la création de la partie');
