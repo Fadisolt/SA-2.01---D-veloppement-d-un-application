4406ddc4899abe71009607a34c76dc74146255e2
idirn <idir.nait-ali@etu.u-paris.fr>
2026-05-14 20:34:30 +0200

    Ajustement des classes ApiService et app pour le début d'une partie. Reglage de la liste déroulante Homonymes. Debut de mise en place d'une partie dans Game.js

--- 2025-S2.01-GettingStarted-main/js/app.js
+++ 2025-S2.01-GettingStarted-main/js/app.js
@@ -4,10 +4,36 @@ import {ApiService} from './ApiService.js';
 
 const domManager = new DOMManager();
 const game = new Game();
+const difficultySelect = document.querySelector('#difficulty');
+const homonymesSelect = document.querySelector('#homonymes');
 
+function updateHomonymesOptions() {
+  const total = parseInt(difficultySelect.value);
+  const options = homonymesSelect.querySelectorAll('option');
+
+  options.forEach(option => {
+    const val = parseInt(option.value);
+
+    if (total % val === 0 && val < total) {
+      option.style.display = "block";
+      option.disabled = false;
+    }
+    else {
+      option.style.display = "none";
+      option.disabled = true;
+    }
+  });
+  const currentVal = parseInt(homonymesSelect.value);
+  if (total % currentVal !== 0 || currentVal >= total) {
+    homonymesSelect.value = "2";
+  }
+}
+
+difficultySelect.addEventListener('change', updateHomonymesOptions);
 
 document.querySelector('.game-form').addEventListener('submit', async function (event) {
   event.preventDefault();
+
   const configGame = {
     playerName: document.querySelector('#playerName').value,
     total: parseInt(document.querySelector('#difficulty').value),
@@ -17,12 +43,15 @@ document.querySelector('.game-form').addEventListener('submit', async function (
   };
 
   try {
-    // Todo Spécifier les paramètres de createGame()
     const data = await ApiService.createGame(configGame.playerName,configGame.total);
-    console.log('Success:', data, data.id);
+
+    console.log("ID reconnu :", data.id);
+    console.log('Success:', data);
     game.startGame(data.id, configGame);
   } catch (error) {
     console.error('Error:', error);
     alert(error.message || 'Erreur lors de la création de la partie');
   }
 });
+
+updateHomonymesOptions();
